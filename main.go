8ee1b89facb897a5fa0a09fa0a0730ed112bfcf7
leijurv <leijurv@gmail.com>
2016-08-26 18:43:17 -0700

    added networking

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"math/rand"
+	"net"
+	"sync"
 	"time"
+)
+
+const (
+	PacketNonceUpdate         uint8 = 4
+	PacketPostContents        uint8 = 7
+	PacketPostContentsRequest uint8 = 9
 )
 
 type Post struct {
@@ -16,7 +26,16 @@ type Post struct {
 	mostRecentNonceUpdate time.Time
 }
 
+type Peer struct {
+	conn      net.Conn
+	writeLock sync.Mutex
+}
+
+var peers []Peer
+var peersLock sync.Mutex
+
 var posts map[[32]byte]Post
+var postsLock sync.Mutex
 
 func (post *Post) payloadHash() [32]byte {
 	return sha256.Sum256(post.payloadRaw)
@@ -70,13 +89,121 @@ func (post *Post) mine(count int) {
 		}
 	}
 }
-func (post *Post) onNewNonceReceived(newNonce [32]byte) {
-	if post.checkPossibleNonce(newNonce) {
-		fmt.Println("Updating post nonce from", hex.EncodeToString(post.nonce[:]), "to", hex.EncodeToString(newNonce[:]))
-		post.nonce = newNonce
-		//send out update here
+func onNonceUpdateReceived(postPayloadHash [32]byte, newNonce [32]byte, peerFrom *Peer) {
+	postsLock.Lock()
+	post, ok := posts[postPayloadHash]
+	
+	if ok {
+		if post.checkPossibleNonce(newNonce) {
+			post.nonce = newNonce // TODO lock post nonce?
+			postsLock.Unlock()
+			fmt.Println("Updating post nonce from", hex.EncodeToString(post.nonce[:]), "to", hex.EncodeToString(newNonce[:]))
+			//send out update here
+		}else{
+			postsLock.Unlock()
+		}
+	} else {
+		postsLock.Unlock()
+		//ask the peer we received this from for the contents of the post because we don't have it
+	}
+
+}
+func onPostContentsReceived(payloadRaw []byte, nonce [32]byte, peerFrom *Peer) {
+	payloadHash := sha256.Sum256(payloadRaw)
+	postsLock.Lock()
+	_, ok := posts[payloadHash]
+	if ok {
+		postsLock.Unlock()
+		//already have it. let's just check if the nonce is better
+		onNonceUpdateReceived(payloadHash, nonce, peerFrom)
+	} else {
+		//dont have it, lets add it
+		now := time.Now()
+		post := Post{
+			payloadRaw:            payloadRaw,
+			nonce:                 nonce,
+			firstReceived:         now,
+			mostRecentNonceUpdate: now,
+		}
+		posts[payloadHash] = post
+		postsLock.Unlock()
+		fmt.Println("Added post with payload hash",post.payloadHash(),"and normal hash",post.hash())
 	}
 }
+func onPostContentsRequested(payloadHash [32]byte, peerFrom *Peer) {
+	postsLock.Lock()
+	post, ok := posts[payloadHash]
+	postsLock.Unlock()
+	if ok {
+		payloadLenBytes:=make([]byte,2)
+		binary.LittleEndian.PutUint16(payloadLenBytes,uint16(len(post.payloadRaw)))
+		fmt.Println("Asking for ",payloadHash)
+		//man I wish go was better at appending mulitple arrays. lol im probbaly doing something wrong here. BUT HEY, IT WORKS
+		message:=append(append(append([]byte{PacketPostContents}, payloadLenBytes...), post.payloadRaw...), post.nonce[:]...)
+		peerFrom.send(message)
+	} else {
+		//um idk we don't have it. just ignore lol
+	}
+}
+func (peer *Peer) send(msg []byte ) error{
+	peer.writeLock.Lock()
+	defer peer.writeLock.Unlock()
+	_,err:= peer.conn.Write(msg)
+	return err
+}
+func (peer *Peer) listen() error {
+	for {
+		msgType := make([]byte, 1)
+		_, err := peer.conn.Read(msgType)
+		if err != nil {
+			return err
+		}
+		switch msgType[0] {
+		case PacketNonceUpdate:
+			payloadHash, err := read32(peer.conn)
+			if err != nil {
+				return err
+			}
+			newNonce, err := read32(peer.conn)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Someone gave us a new nonce",newNonce,"for",payloadHash)
+			go onNonceUpdateReceived(payloadHash, newNonce, peer)
+		case PacketPostContents:
+			payloadLenBytes := make([]byte, 2)
+			_, err := io.ReadFull(peer.conn, payloadLenBytes)
+			if err != nil {
+				return err
+			}
+			payloadLen := int16(binary.LittleEndian.Uint16(payloadLenBytes))
+			payload := make([]byte, payloadLen)
+			_, err = io.ReadFull(peer.conn, payloadLenBytes)
+			if err != nil {
+				return err
+			}
+			nonce, err := read32(peer.conn)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Someone gave us post contents")
+			go onPostContentsReceived(payload, nonce, peer)
+		case PacketPostContentsRequest:
+			requestedPayloadHash, err := read32(peer.conn)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Someone just asked us for post contents for payload hash",requestedPayloadHash)
+			go onPostContentsRequested(requestedPayloadHash, peer)
+		}
+	}
+}
+func read32(conn net.Conn) ([32]byte, error) {
+	var data [32]byte
+	_, err := io.ReadFull(conn, data[:])
+	return data, err
+}
+
 func main() {
 	toMine := Post{payloadRaw: []byte("wewlad")}
 	fmt.Println(toMine.hash())
