bff29fa0c5a2aa4c5c4badbf279d3613e582b094
Howard Stark <howard@getcoffee.io>
2017-06-25 17:51:26 -0700

    Added disk payload cache backing option

--- postmanager.go
+++ postmanager.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"encoding/hex"
 	"errors"
+	"math"
+	"os"
+	"path/filepath"
 	"sync"
 )
 
 var (
-	ErrPayloadExists  error = errors.New("payloadcache: writing payload that already exists")
-	ErrPayloadMissing error = errors.New("payloadcache: fetching payload that does not exist")
+	ErrPayloadExists    error = errors.New("payloadcache: writing payload that already exists")
+	ErrPayloadMissing   error = errors.New("payloadcache: fetching payload that does not exist")
+	ErrPayloadOversized error = errors.New("payloadcache: reading a payload that is too large (payload > 2^16 bytes)")
 
 	ErrPostMissing error = errors.New("postcache: fetching post that does not exist")
 )
@@ -43,14 +48,14 @@ func (manager *PostManager) FetchPayload(post *Post) error {
 }
 
 type MemoryPostCache struct {
-	Lock    sync.RWMutex
-	Content map[PayloadHash]*Post
+	lock    sync.RWMutex
+	content map[PayloadHash]*Post
 }
 
 func (cache *MemoryPostCache) GetPost(hash PayloadHash) (*Post, error) {
-	cache.Lock.RLock()
-	defer cache.Lock.RUnlock()
-	post, ok := cache.Content[hash]
+	cache.lock.RLock()
+	defer cache.lock.RUnlock()
+	post, ok := cache.content[hash]
 	if !ok {
 		return post, ErrPostMissing
 	}
@@ -58,21 +63,21 @@ func (cache *MemoryPostCache) GetPost(hash PayloadHash) (*Post, error) {
 }
 
 func (cache *MemoryPostCache) WritePost(hash PayloadHash, post *Post) error {
-	cache.Lock.Lock()
-	defer cache.Lock.Unlock()
-	cache.Content[hash] = post
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+	cache.content[hash] = post
 	return nil
 }
 
 type MemoryPayloadCache struct {
-	Lock    sync.RWMutex
-	Content map[PayloadHash]*Payload
+	lock    sync.RWMutex
+	content map[PayloadHash]*Payload
 }
 
 func (cache *MemoryPayloadCache) GetPayload(hash PayloadHash) (*Payload, error) {
-	cache.Lock.RLock()
-	defer cache.Lock.RUnlock()
-	payload, ok := cache.Content[hash]
+	cache.lock.RLock()
+	defer cache.lock.RUnlock()
+	payload, ok := cache.content[hash]
 	if !ok {
 		return nil, ErrPayloadMissing
 	}
@@ -80,18 +85,94 @@ func (cache *MemoryPayloadCache) GetPayload(hash PayloadHash) (*Payload, error) 
 }
 
 func (cache *MemoryPayloadCache) HasPayload(hash PayloadHash) bool {
-	cache.Lock.RLock()
-	defer cache.Lock.RUnlock()
-	_, ok := cache.Content[hash]
+	cache.lock.RLock()
+	defer cache.lock.RUnlock()
+	_, ok := cache.content[hash]
 	return ok
 }
 
 func (cache *MemoryPayloadCache) WritePayload(hash PayloadHash, payload *Payload) error {
-	cache.Lock.Lock()
-	defer cache.Lock.Unlock()
-	if _, ok := cache.Content[hash]; ok {
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+	if _, ok := cache.content[hash]; ok {
 		return ErrPayloadExists
 	}
-	cache.Content[hash] = payload
+	cache.content[hash] = payload
+	return nil
+}
+
+type DiskPayloadCache struct {
+	lock    sync.RWMutex
+	content map[PayloadHash]*Payload
+}
+
+func GetHashPath(hash PayloadHash) string {
+	return filepath.Join(hex.EncodeToString(hash[:1]), hex.EncodeToString(hash[1:]))
+}
+
+func (cache *DiskPayloadCache) GetPayload(hash PayloadHash) (*Payload, error) {
+	if !cache.HasPayload(hash) {
+		return nil, ErrPayloadMissing
+	}
+	cache.lock.Lock()
+	defer cache.lock.Unlock()
+	if payload, ok := cache.content[hash]; ok {
+		return payload, nil
+	}
+	file, err := os.Open(GetHashPath(hash))
+	if err != nil {
+		return nil, err
+	}
+	defer func(file *os.File) {
+		if err := file.Close(); err != nil {
+			Error.Println(err)
+			return
+		}
+		// TODO: Maybe add some extra shutdown logic.
+	}(file)
+	payload := Payload{}
+	read, err := file.Read(payload)
+	if err != nil {
+		return nil, err
+	}
+	if read > math.MaxUint16 {
+		return nil, ErrPayloadOversized
+	}
+	cache.content[hash] = &payload
+	return &payload, nil
+}
+
+func (cache *DiskPayloadCache) HasPayload(hash PayloadHash) bool {
+	cache.lock.RLock()
+	defer cache.lock.RUnlock()
+	_, ok := cache.content[hash]
+	if ok {
+		return ok
+	}
+	if _, err := os.Stat(GetHashPath(hash)); err == nil {
+		return true
+	}
+	return false
+}
+
+func (cache *DiskPayloadCache) WritePayload(hash PayloadHash, payload *Payload) error {
+	if cache.HasPayload(hash) {
+		return ErrPayloadExists
+	}
+	file, err := os.OpenFile(GetHashPath(hash), os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+	defer func(file *os.File) {
+		if err := file.Close(); err != nil {
+			Error.Println(err)
+			return
+		}
+		// TODO: Maybe add some extra shutdown logic.
+	}(file)
+	_, err = file.Write(*payload)
+	if err != nil {
+		return err
+	}
 	return nil
 }
